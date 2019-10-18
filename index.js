56925461cacc92d6835463bbba84fe44a668689f
hdf1986 <hugo.farji+github@gmail.com>
2019-10-18 19:45:25 -0300

    Added basic bot integration

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.PORT || 3000
+const TELEGRAM_API_TOKEN = process.env.TELEGRAM_API_TOKEN || ''
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/webhooks/telegram', (req, res) => {
+  console.log(req.body)
+  
+  res.send({ status: "ok" })
+})
 
 app.listen(port, () => console.log(`Weatherbot app listening on port ${port}!`))
