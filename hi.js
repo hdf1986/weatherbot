382ea35585683cf8fde0c5ffd6488ef81a04192f
hdf1986 <hugo.farji+github@gmail.com>
2019-10-18 21:12:26 -0300

    Debugging bot

--- hi.js
+++ hi.js
@@ -2,9 +2,22 @@ const User = require('./models/user');
 const sendMessage = require('./utils/telegram');
 const getWeather = require('./utils/openweather');
 
+const howItsToday = (weather) => {
+  const todayDate = new Date()
+  const today = `${todayDate.getFullYear()}-${todayDate.getMonth()}-${todayDate.getDate()}`
+  const weatherOfToday = weather.list.map(w => w.dt_txt)
+                                     .filter((i) => i.startsWith(today))
+  console.log(weatherOfToday)
+}
+
 User.findAll().then(users => {
   users.forEach(user => {
     console.log(`Saying hi to ${user.id}`)
+
+    const [lat, lon] = user.latLon.split(',')
+    getWeather({
+      lat, lon
+    }).then(() => howItsToday(weather))
     sendMessage({
       conversationId: user.conversationId,
       text: "Hola"
