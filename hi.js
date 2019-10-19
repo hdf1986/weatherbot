402eeea34ec303e1b315a53ab6cad4f737d5a1d4
hdf1986 <hugo.farji+github@gmail.com>
2019-10-18 21:29:13 -0300

    Debugging bot

--- hi.js
+++ hi.js
@@ -11,7 +11,7 @@ const howItsToday = (weather) => {
   const todayDate = new Date()
   const today = `${todayDate.getFullYear()}-${todayDate.getMonth() + 1}-${todayDate.getDate()}`
   const weatherOfToday = weather.list.filter((i) => i.dt_txt.startsWith(today))
-
+  console.log(weatherOfToday)
   if(weatherOfToday.includes(w => w[0].main === "rain" || w[0].main === "thunderstorm")) {
     return 'rain'
   } else if(weatherOfToday.includes(w => w.main.temp_min < 100)) {
